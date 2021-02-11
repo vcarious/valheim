93151e9c5fa5d6daaa1cf67fcf500f1e0d545dfb
Alex Romano <aromano937@gmail.com>
2021-02-10 21:10:22 -0800

    addining init script for post create installs

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo su root
+sudo apt-get update -y
+# need to fix permission issue later and not run everything in root
+# sudo useradd -m steam
+# cd /home/steam
+
+sudo apt-get upgrade -y
+sudo dpkg --add-architecture i386
+sudo apt-get update -y
+sudo apt-get install lib32gcc1 steamcmd -y
+
+steamcmd +login anonymous +force_install_dir /home/steam/valheim +app_update 896660 +quit -y
+
+export templdpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+export SteamAppId=892970
+
+cd /home/steam/valheim/
+echo "starting server PRESS CTRL-C to exit"
+./valheim_server.x86_64 -name "BucBucGo" -port 2456 -world "bbg1" -password "thecan123"
+export LD_LIBRARY_PATH=$templdpath
