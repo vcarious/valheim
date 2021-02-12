999f7417eee1c945b4cef3136765b9d2ca5b026f
Alex Romano <aromano937@gmail.com>
2021-02-12 10:39:04 -0800

    updating restore file

--- utilities/restore_save.sh
+++ utilities/restore_save.sh
@@ -5,8 +5,7 @@
 # for the first time, it will notice the saves and not create a new world loading those instead
 
 # TODO: optimize.  this is sloppy...
-sudo su root
-mkdir ~/.config/unity3d/unity3d/IronGate/Valheim/worlds
+mkdir -p  ~/.config/unity3d/IronGate/Valheim/worlds
 
 cp /home/ubuntu/bbg1.fwl /root/.config/unity3d/IronGate/Valheim/worlds/
 cp /home/ubuntu/bbg1.db /root/.config/unity3d/IronGate/Valheim/worlds/
